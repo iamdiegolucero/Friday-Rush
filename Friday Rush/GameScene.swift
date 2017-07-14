ddf5e0747ae317049811fdf419d8a06819a73d82
smonametsi <smonametsi@gmail.com>
2017-07-14 09:52:28 -0500

    The obstacle
    I added the obstacle, you may want to color it to make it look like a closet or something. I also added the code from hoppy bunny regarding the obstacles. We can alter it as we see fit.

--- Friday Rush/GameScene.swift
+++ Friday Rush/GameScene.swift
@@ -12,9 +12,33 @@ import GameplayKit
 class GameScene: SKScene {
     var ground: SKReferenceNode!
     var building: SKReferenceNode!
+    var obstacleSource: SKNode!
+    var obstacleLayer: SKNode!
     
     override func didMove(to view: SKView) {
         ground = self.childNode(withName: "ground") as!SKReferenceNode
         building = self.childNode(withName: "building") as! SKReferenceNode
+        obstacleLayer = self.childNode(withName: "obstacleLayer")
     }
+}
+
+func updateObstacles() {
+    /* Update Obstacles */
+    
+    obstacleLayer.position.x -= scrollSpeed * CGFloat(fixedDelta)
+    
+    /* Loop through obstacle layer nodes */
+    for obstacle in obstacleLayer.children as! [SKReferenceNode] {
+        
+        /* Get obstacle node position, convert node position to scene space */
+        let obstaclePosition = obstacleLayer.convert(obstacle.position, to: self)
+        
+        /* Check if obstacle has left the scene */
+        if obstaclePosition.x <= -26 {
+            // 26 is one half the width of an obstacle
+            
+            /* Remove obstacle node from obstacle layer */
+            obstacle.removeFromParent()
+        }
+        
 }
